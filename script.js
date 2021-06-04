22692d69459e843b0918e0ac1dd2a18377c9eafe
Abdisalan <abdisalanvp77@gmail.com>
2021-06-04 01:41:15 -0400

    added the data from api

---
+++ script.js
@@ -0,0 +1,53 @@
+const searchEl = document.querySelector('.searchbox')
+const div = document.querySelector('.users-list');
+let usersList;
+
+// showUsers(usersList);
+fetchData();
+
+searchEl.addEventListener('keyup', (e) => {
+    let searcTerm = e.target.value;
+    filterUsers(searcTerm);
+});
+
+async function fetchData() {
+    const res =  await fetch('https://randomuser.me/api?results=50');
+    const { results } = await res.json();
+    // clear result
+    showUsers(results);
+    usersList = results;
+}
+
+function filterUsers(searcTerm) {
+
+    div.innerHTML = '';
+
+    let filteredUsers = usersList.filter(user => {
+        return user.name.first.toLowerCase().indexOf(searcTerm.toLowerCase()) !== -1 || user.name.last.toLowerCase().indexOf(searcTerm.toLowerCase()) !== -1 || user.location.city.toLowerCase().indexOf(searcTerm.toLowerCase()) !== -1;
+    });
+
+    showUsers(filteredUsers);
+
+}
+
+function showUsers(users) {
+    users.forEach(user => displayUser(user));
+}
+
+
+function displayUser(data) {
+    let li = document.createElement('li');
+    li.innerHTML = `
+    <img
+        src="${data.picture.large}"
+        alt="${data.name.first}"
+    />
+    <div class="user-info">
+        <h4>${data.name.first + " " + data.name.last}</h4>
+        <p>${data.location.city + ", " + data.location.country}</p>
+    </div>
+    
+    `;
+
+    div.appendChild(li);
+}
